3acc585f3fcf637c19813bb9d43cc44bd1bbc531
EddyTheDove <eddy.insearch@gmail.com>
2017-09-23 19:52:09 +1000

    allow fullscreen youtube

--- resources/views/front/videos/youtube.blade.php
+++ resources/views/front/videos/youtube.blade.php
@@ -1,3 +1,8 @@
 <div class="embed-responsive embed-responsive-16by9">
-    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{ $main->origin_id }}"></iframe>
+    <iframe
+        class="embed-responsive-item"
+        src="https://www.youtube.com/embed/{{ $main->origin_id }}"
+        frameborder="0"
+        allowfullscreen>
+    </iframe>
 </div>
